d72c4ddf1e8878b048c3c78f8d175580c42d797b
ure-asuslaptop <urosr95@gmail.com>
2020-07-14 13:31:02 +0200

    fixed team create error handling

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -9,7 +9,7 @@ import { observer } from "mobx-react";
 // import { useLoginStoreContext } from "../stores/LoginStore";
 import { loginSchema } from "../constants/validationSchema";
 import { TextInput } from "../shared-components/TextInput";
-import { WELCOME_ROUTE } from "../constants/routes";
+import { TEAM_HOME_ROUTE } from "../constants/routes";
 
 const loginQuery = gql`
   mutation Login($email: String!, $password: String!) {
@@ -50,7 +50,7 @@ export const Login: React.FC<Props> = observer(() => {
           const { ok, errors, token, refreshToken } = data.login;
 
           if (ok) {
-            history.push(WELCOME_ROUTE);
+            history.push(TEAM_HOME_ROUTE);
             localStorage.setItem("token", token);
             localStorage.setItem("refreshToken", refreshToken);
           } else {
